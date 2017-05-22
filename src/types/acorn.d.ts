b7a777c8bd0e93249840539e0b2806a14fd65ea0
Matt Andrews <mattandrews@massey.com.au>
2017-05-22 16:22:25 +1000

    Changed TypeScript typings

--- src/types/acorn.d.ts
+++ src/types/acorn.d.ts
@@ -1,13 +1,1 @@
-/*~ If there are types, properties, or methods inside dotted names
- *~ of the module, declare them inside a 'namespace'.
- */
-export namespace acorn {
-    /*~ For example, given this definition, someone could write:
-     *~   import { subProp } from 'yourModule';
-     *~   subProp.foo();
-     *~ or
-     *~   import * as yourMod from 'yourModule';
-     *~   yourMod.subProp.foo();
-     */
-    export function parse(script: string): object;
-}
+export function parse(script: string): object;
